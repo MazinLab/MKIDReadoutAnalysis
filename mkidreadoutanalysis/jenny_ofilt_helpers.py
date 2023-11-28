a5509fd03223450ce30e50be37efdc24498aac15
Jenny Smith <jennifer_smith@ucsb.edu>
2023-11-27 18:18:19 -0800

    clean up debugging scripts

--- mkidreadoutanalysis/jenny_ofilt_helpers.py
+++ mkidreadoutanalysis/jenny_ofilt_helpers.py
@@ -2,6 +2,19 @@ import logging
 
 import matplotlib.pyplot as plt
 from mkidreadoutanalysis.resonator import *
+from mkidreadoutanalysis.mkidreadout import *
+
+
+def shift_and_normalize(template, ntemplate, offset):
+    template = template.copy()
+    if template.min() != 0:  # all weights could be zero
+        template /= np.abs(template.min())  # correct over all template height
+
+    # shift template (max may not be exactly at offset due to filtering and imperfect default template)
+    start = 10 + np.argmin(template) - offset
+    stop = start + ntemplate
+    template = np.pad(template, 10, mode='wrap')[start:stop]  # use wrap to not change the frequency content
+    return template
 
 
 def generate_fake_data(res: Resonator = Resonator(f0=4.0012e9, qi=200000, qc=15000, xa=1e-9, a=0, tls_scale=1), fs=1e6,
@@ -10,14 +23,14 @@ def generate_fake_data(res: Resonator = Resonator(f0=4.0012e9, qi=200000, qc=150
                        noise=True, noise_scale=10, line_noise_freqs=[60, 50e3, 100e3, 250e3, -300e3, 300e3, 500e3],
                        line_noise_amplitudes=[0.005, 0.001, 0.005, 0.003, 0.001, 0.005, 0.001],
                        line_noise_phases=[0, 0.5, 0, 1.3, 0.5, 0.2, 2.4]):
-    quasiparticle_timestream = QuasiparticleTimeStream(fs = 1e6, ts = 10)
+    quasiparticle_timestream = QuasiparticleTimeStream(fs=1e6, ts=10)
     quasiparticle_timestream.gen_quasiparticle_pulse(tf=30);
     quasiparticle_timestream.gen_photon_arrivals(cps=1551)
     quasiparticle_timestream.populate_photons()
     # Create resonator and compute S21
-    resonator = res #1e2
+    resonator = res  # 1e2
     rf = rf
-    freq = FrequencyGrid( fc=res.f0_0, points=1000, span=500e6)
+    freq = FrequencyGrid(fc=res.f0_0, points=1000, span=500e6)
     sweep = ResonatorSweep(resonator, freq, rf)
     lit_res_measurment = ReadoutPhotonResonator(resonator, quasiparticle_timestream, freq, rf)
     # toggle white noise and line noise
@@ -27,7 +40,7 @@ def generate_fake_data(res: Resonator = Resonator(f0=4.0012e9, qi=200000, qc=150
     lit_res_measurment.rf.noise_scale = noise_scale
 
     # configure line noise
-    lit_res_measurment.rf.line_noise.freqs = line_noise_freqs # Hz and relative to center of bin (MKID we are reading out)
+    lit_res_measurment.rf.line_noise.freqs = line_noise_freqs  # Hz and relative to center of bin (MKID we are reading out)
     lit_res_measurment.rf.line_noise.amplitudes = line_noise_amplitudes
     lit_res_measurment.rf.line_noise.phases = line_noise_phases
 
@@ -35,3 +48,73 @@ def generate_fake_data(res: Resonator = Resonator(f0=4.0012e9, qi=200000, qc=150
 
     return phase_data
 
+
+def ofilt_summary_plot(psd, dt, noise_nwindow, filter, template):
+
+    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(9, 9))
+    # PSD
+    ax[0,0].set_xlabel("frequency [Hz]")
+    ax[0,0].set_ylabel("PSD [dBc / Hz]")
+    f = np.fft.rfftfreq(noise_nwindow, d=dt)
+    ax[0,0].semilogx(f, 10 * np.log10(psd))
+    # FILTER
+    ax[0,1].set_xlabel(r"time [$\mu$s]")
+    ax[0,1].set_ylabel("filter coefficient [radians]")
+    ax[0,1].plot(np.arange(filter.size) * dt * 1e6, filter)
+    # TEMPLATE
+    ax[1,0].set_xlabel(r"time [$\mu$s]")
+    ax[1,0].set_ylabel("template [arb.]")
+    ax[1,0].plot(np.arange(template.size) * dt * 1e6, template)
+    ax[1, 0].plot(np.arange(filter.size) * dt * 1e6, template[:filter.size])
+    plt.show()
+
+def ofilt_plot_comparison(phase_data: np.ndarray, readout: MKIDReadout, phase_ofilt: np.ndarray,
+                          readout_ofilt: MKIDReadout, data_key: str, xlim: (int, int) = (60000, 100000)):
+    """
+    Comparison plot for phase data before and after filtering.
+    Args:
+        phase_data: raw phase data
+        readout: MKIDReadout object containing trigger locations, energies, etc. for raw data
+        phase_ofilt: filtered phase data
+        readout_ofilt: MKIDReadout object containing trigger locations, energies, etc. for filtered data
+        data_key: 'red', 'blue', or 'ir' for plot labeling and color coding
+        xlim: timeseries plot x limits
+
+    Returns:
+        2x2 plot showing time series and resulting phase histograms for filtered and unfiltered data.
+
+    """
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10), sharex='row', sharey='row')
+    fig.suptitle("Jenny's Ofilt")
+    tvec = np.arange(phase_data.size)
+    if data_key == 'ir':
+        data_color = 'lightcoral'
+    else:
+        data_color = data_key
+
+    axs[0, 0].plot(tvec[xlim[0]:xlim[1]], phase_data[xlim[0]:xlim[1]], label='phase timestream', color=data_color)
+    axs[0, 0].plot(np.ma.masked_outside(tvec[readout.trig], xlim[0], xlim[1]), phase_data[readout.trig], '.',
+                   label='trigger')
+    axs[0, 0].set_xlabel('time (us)')
+    axs[0, 0].set_ylabel('phase (radians)')
+    axs[0, 0].plot(np.ma.masked_outside(tvec[readout.photon_energy_idx], xlim[0], xlim[1]),
+                   phase_data[readout.photon_energy_idx], 'o', label='energy')
+    axs[0, 0].legend(loc='upper right')
+
+    axs[0, 0].set_title("Raw Data")
+    axs[0, 1].plot(tvec[xlim[0]:xlim[1]], phase_ofilt[xlim[0]:xlim[1]], label='phase timestream', color=data_color)
+    axs[0, 1].plot(np.ma.masked_outside(tvec[readout_ofilt.trig], xlim[0], xlim[1]), phase_ofilt[readout_ofilt.trig],
+                   '.', label='trigger')
+    axs[0, 1].set_xlabel('time (us)')
+    axs[0, 1].set_ylabel('phase (radians)')
+    axs[0, 1].plot(np.ma.masked_outside(tvec[readout_ofilt.photon_energy_idx], xlim[0], xlim[1]),
+                   phase_ofilt[readout_ofilt.photon_energy_idx], 'o', label='energy')
+    axs[0, 1].set_title("Filtered Data")
+    axs[0, 1].legend(loc='upper right')
+    axs[1, 0].hist(readout.photon_energies, color=data_color, bins='auto', density=True)
+    axs[1, 0].set_xlabel('Phase peak')
+    axs[1, 0].set_ylabel('Counts')
+    axs[1, 1].hist(readout_ofilt.photon_energies, color=data_color, bins='auto', density=True)
+    axs[1, 1].set_xlabel('Phase peak')
+    axs[1, 1].set_ylabel('Counts')
+    plt.show()
--- ofilt_test_script.py
+++ ofilt_test_script.py
@@ -10,25 +10,20 @@ from mkidreadoutanalysis.mkidreadout import MKIDReadout
 from mkidreadoutanalysis.optimal_filters.make_filters import Calculator
 from mkidreadoutanalysis.optimal_filters.config import ConfigThing
 import scipy as sp
+from mkidreadoutanalysis.jenny_ofilt_helpers import generate_fake_data, ofilt_plot_comparison
 
 
 
+## IMPORT / GENERATE DATA
+data_key = 'blue' # options: 'red', 'ir', 'None' (generates fake data)
+if data_key == 'None':
+    phase_data = generate_fake_data()
+else:
+    data = np.load(f'/work/jpsmith/Gen3/Fridge_Tests/r_testing/data/white_fridge/10_18_23/wf_ellison3_6000_650GHz.npz')
+    phase_data = data[data_key+'_phase'] * np.pi
+    del data
 
-data = np.load(f'/work/jpsmith/Gen3/Fridge_Tests/r_testing/data/white_fridge/10_18_23/wf_ellison3_6000_650GHz.npz')
-blue_phase=data['red_phase']*np.pi
-del data
-
-#blue_phase_readout = MKIDReadout()
-#blue_phase_readout.trigger(blue_phase, fs = 1e6, threshold=-1, deadtime=60)
-#plt.xlim([60000,1000000]);
-#x = slice(16000, 19000)
-#blue_phase_dark_mean = blue_phase[x].mean()
-#blue_phase_max_location, blue_phase_fwhm = compute_r(blue_phase_readout.photon_energies - blue_phase_dark_mean, color='blue', plot=False)
-#print(f'Max Phase: {-blue_phase_ofilt_max_location} FWHM: {blue_phase_ofilt_fwhm} radians')
-#plt.title('Blue Photons Optimal Filter (409.5 nm), FPGA Phase');
-#plt.show()
-
-
+## CONFIG PARAMETERS
 cfg=ConfigThing()
 cfg.registerfromkvlist((('dt', 1e-6),
 ('fit', False),
@@ -39,18 +34,17 @@ cfg.registerfromkvlist((('dt', 1e-6),
 ('pulses.fallback_template', None), # specify a pre-computed fallback template,
 
 ('pulses.tf', 30), # pre filter pulse fall time in microseconds
-('pulses.ntemplate', 5000), # need to set this larger to calculate covariance matrix in the time domain "accurately" for the number of selected filter coefficients
-('pulses.offset', 10),
+('pulses.ntemplate', 50), # need to set this larger to calculate covariance matrix in the time domain "accurately" for the number of selected filter coefficients
+('pulses.offset', 5),
 ('pulses.threshold', 6), # sigma above noise
 ('pulses.separation', 500),
 ('pulses.min_pulses', 10000),
-('noise.nwindow', 1000), #1000
+('noise.nwindow', 50), #1000
 ('noise.isolation', 200),
 ('noise.max_windows', 2000), # maximum number of nwindows of samples needed before moving on [int]
-('noise.max_noise', 2000), #2000
+#('noise.max_noise', 2000), #2000
 ('template.percent', 80),
-('template.cutoff', .2),
-('filter.cutoff', .5),
+('filter.cutoff', .1),
 ('template.min_tau', 5),
 ('template.max_tau', 100),
 ('template.fit', 'triple_exponential'),
@@ -59,51 +53,29 @@ cfg.registerfromkvlist((('dt', 1e-6),
                         # need to make sure filter is periodic and this gets hard when the filter is short
 ('filter.normalize', True)), namespace='')
 
-ofc = Calculator(phase=blue_phase, config=cfg, name='simulated')
+ofc = Calculator(phase=phase_data, config=cfg, name='simulated')
 
 ofc.calculate(clear=False)
-plt.show()
+ofc.plot(); plt.show();
 
-blue_phase_ofilt = sp.signal.convolve(blue_phase, ofc.result['filter'], mode='same')
-blue_phase_readout_ofilt = MKIDReadout()
-blue_phase_readout_ofilt.trigger(blue_phase_ofilt, fs = 1e6, threshold=-1.2, deadtime=60)
-x = slice(16000, 19000)
-blue_phase_ofilt_dark_mean = blue_phase_ofilt[x].mean()
-blue_phase_ofilt_max_location, blue_phase_ofilt_fwhm = compute_r(blue_phase_readout_ofilt.photon_energies - blue_phase_ofilt_dark_mean, color='blue', plot=False)
+optimal_filter = ofc.result['filter']
+
+## APPLY FILTER
+phase_ofilt = sp.signal.convolve(phase_data,optimal_filter, mode='same')
+readout_ofilt = MKIDReadout()
+readout_ofilt.trigger(phase_ofilt, fs = 1e6, threshold=-1.2, deadtime=60)
+phase_ofilt_dark_mean = phase_data[25000:60000].mean() #TODO: Make automatic
+phase_ofilt_max_location, phase_ofilt_fwhm = compute_r(readout_ofilt.photon_energies - phase_ofilt_dark_mean, color='blue', plot=False)
 #print(f'Max Phase: {-blue_phase_ofilt_max_location} FWHM: {blue_phase_ofilt_fwhm} radians')
 #plt.title('Blue Photons Optimal Filter (409.5 nm), FPGA Phase');
 #plt.show()
 
+plot_comparison=True
+if plot_comparison:
+    ## PROCESS W/O OFILT
+    readout = MKIDReadout()
+    readout.trigger(phase_data, fs=1e6, threshold=0.0, deadtime=60)
+    readout.plot_triggers(phase_data, energies=True, xlim=(60000, 100000));
+    plt.show();
+    ofilt_plot_comparison(phase_data, readout, phase_ofilt, readout_ofilt, data_key, xlim=(60000, 100000))
 
-
-fig, axs = plt.subplots(2,2, figsize=(10,10))
-fig.suptitle("Nic's Ofilt")
-tvec = np.arange(blue_phase.size)
-axs[0,0].plot(tvec, blue_phase, label='phase timestream', color='blue')
-axs[0,0].plot(tvec[blue_phase_readout.trig], blue_phase[blue_phase_readout.trig], '.', label='trigger')
-axs[0,0].set_xlabel('time (us)')
-axs[0,0].set_ylabel('phase (radians)')
-axs[0,0].plot(tvec[blue_phase_readout.photon_energy_idx], blue_phase[blue_phase_readout.photon_energy_idx], 'o', label='energy')
-axs[0,0].set_title("Raw Data")
-axs[0,0].set_xlim(60000,1000000)
-axs[0,0].set_ylim(-4, 3.15)
-axs[0,1].plot(tvec, blue_phase_ofilt, label='phase timestream', color='blue')
-axs[0,1].plot(tvec[blue_phase_readout_ofilt.trig], blue_phase_ofilt[blue_phase_readout_ofilt.trig], '.', label='trigger')
-axs[0,1].set_xlabel('time (us)')
-axs[0,1].set_ylabel('phase (radians)')
-axs[0,1].plot(tvec[blue_phase_readout_ofilt.photon_energy_idx], blue_phase_ofilt[blue_phase_readout_ofilt.photon_energy_idx], 'o', label='energy')
-axs[0,1].set_title("Filtered Data")
-axs[0,1].set_xlim(60000,1000000)
-axs[0,1].set_ylim(-4, 3.15)
-axs[1,0].hist(blue_phase_readout.photon_energies, color='blue', bins='auto', density=True, label=f'Center: {blue_phase_max_location:1f} FWHM:{blue_phase_fwhm:1f}')
-axs[1,0].set_xlabel('Phase peak')
-axs[1,0].legend(loc='upper right')
-axs[1,0].set_ylabel('Counts')
-axs[1,1].hist(blue_phase_readout_ofilt.photon_energies, color='blue', bins='auto', density=True, label=f'Center: {blue_phase_ofilt_max_location:1f} FWHM:{blue_phase_ofilt_fwhm:1f}')
-axs[1,1].set_xlabel('Phase peak')
-axs[1,1].set_ylabel('Counts')
-axs[1,1].set_xlim(-6, 0)
-axs[1,1].set_ylim(0, 9)
-axs[1,1].legend(loc='upper right')
-plt.show()
-print('hi')
